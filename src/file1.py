2ed3b0ab7982742b698cba1ca943b62b259820e5
Seenu-Dasetty <dasettyseenu@gmail.com>
2025-11-01 10:00:45 +1300

    modified the code

--- src/file1.py
+++ src/file1.py
@@ -6,6 +6,11 @@
 
 # print(len(input("what is your name?")))
 
-username = input("what is your name? ")
-length = len(username)
-print(length)
+# username = input("what is your name? ")
+# length = len(username)
+# print(length)
+
+print("Welcome to the Band Name Generator")
+city = input("what's the name of the city you grow up in? \n")
+pet_name = input("what's your pet name? \n")
+print("Your band could be " + city +"\t"+ pet_name )
