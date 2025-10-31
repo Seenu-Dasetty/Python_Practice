c1e8abc7fa10d566644c537bd33a19d8279ff1b9
Seenu-Dasetty <dasettyseenu@gmail.com>
2025-11-01 09:34:41 +1300

    modified the code

--- src/file1.py
+++ src/file1.py
@@ -1,2 +1,11 @@
 # print("Hello Seenu \n"  "how are you")
-print("Hello " + input("what is your name?") + "!")
+# print("Hello  " + input("what is your name?") + "!")
+
+# name = "Seenu"
+# print(len(name))
+
+# print(len(input("what is your name?")))
+
+username = input("what is your name? ")
+length = len(username)
+print(length)
