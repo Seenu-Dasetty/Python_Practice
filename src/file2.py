03dd1c3c834a8b95de2556fa328864f7c78aa560
Seenu-Dasetty <dasettyseenu@gmail.com>
2025-11-01 15:27:03 +1300

    modified the code

---
+++ src/file2.py
@@ -0,0 +1,24 @@
+# print("hello"[4])
+# print(type(12344))
+
+
+# name = input("enter your name ")
+# length_of_name = len(name)
+# print("number of letters in your name: " + str(length_of_name))
+
+
+# height = 1.65
+# weight = 84
+#
+# bmi = weight/(height*height)
+#
+# print(bmi)
+
+
+score = 100
+height = 5.7
+is_winning = True
+
+print(f"your score is: {score}, your height is: {height} and you are winning is {is_winning} ")
+
+
